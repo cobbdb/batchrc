df781cd05f16611fb4a1088253e0371b3bfb2dbb
cobbdb <cobbdb@gmail.com>
2013-04-25 23:20:50 -0700

    Better readability on filecount.

--- filecount.bat
+++ filecount.bat
@@ -9,4 +9,4 @@ if "%1"=="/?" (
 )
 
 :: http://social.technet.microsoft.com/wiki/contents/articles/9173.counting-the-number-of-files-in-a-directory-command-line-style.aspx
-dir/s/b/a-d | find /v /c "::"
+dir /s /b /a-d | find /v /c "::"
