1e5ea47695be5d85b5c3999ecb19cb7e1e477c43
cobbdb <cobbdb@gmail.com>
2013-05-03 10:06:36 -0700

    Another awesome Tunnelier feature.

---
+++ sftp.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if [%1]==[] (
+    echo Missing destination. Use /? for help.
+    goto:eof
+)
+if "%1" == "/?" (
+    echo sftp profileName^|user@host.com
+    goto:eof
+)
+
+:: http://www.bitvise.com/tunnelier#sftpc
+set profile=F:\Documents\SSHProfiles\%1.bscp
+if exist %path% (
+    sftpc -profile=%profile%
+) else (
+    sftpc %1
+)
